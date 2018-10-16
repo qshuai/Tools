0bc3f5d7f8d28010d58ad94b2e431bc00b8c9f91
qshuai <qishuai231@gmail.com>
2018-10-16 22:49:29 +0800

    Add calculate transaction hash tool

---
+++ calcHash/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"fmt"
+
+	"github.com/bcext/gcash/chaincfg/chainhash"
+	"github.com/qshuai/tcolor"
+)
+
+func main() {
+	rawtx := flag.String("rawtx", "", "Please input the raw hexadecimal transaction string")
+	flag.Parse()
+	if rawtx == nil {
+		fmt.Println(tcolor.WithColor(tcolor.Red, "lack of raw hexadecimal transaction string"))
+		return
+	}
+
+	b, err := hex.DecodeString(*rawtx)
+	if err != nil {
+		fmt.Println(tcolor.WithColor(tcolor.Red, "not valid hexadecimal transaction string"))
+		return
+	}
+
+	txHash := chainhash.DoubleHashH(b)
+	fmt.Println(tcolor.WithColor(tcolor.Green, "txhash: "+txHash.String()))
+}

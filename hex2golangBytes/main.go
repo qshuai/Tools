69af18a40b27a82cd9349f22df957af05e0bef06
qshuai <qishuai231@gmail.com>
2018-12-06 15:48:29 +0800

    Support break a new line

--- hex2golangBytes/main.go
+++ hex2golangBytes/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	data := flag.String("data", "", "please a hexadecimal string")
+	num := flag.Int("num", 12, "bytes per line")
 	flag.Parse()
 
 	if len(*data) == 0 {
@@ -22,11 +23,28 @@ func main() {
 		panic(err)
 	}
 
-	for _, item := range b {
+	var lineOffset int
+	fmt.Print("[]byte{")
+	for index, item := range b {
 		if item <= 0x0f {
-			fmt.Printf("0x0%x, ", item)
+			fmt.Printf("0x0%x", item)
 		} else {
-			fmt.Printf("%#x, ", item)
+			fmt.Printf("%#x", item)
+		}
+
+		if index != len(b)-1 {
+			fmt.Print(", ")
+		}
+
+		lineOffset++
+		if lineOffset%*num == 0 {
+			fmt.Print("\n")
+		}
+
+		if (index < *num-2) && lineOffset%(*num-2) == 0 {
+			fmt.Print("\n")
+			lineOffset = 0
 		}
 	}
+	fmt.Print("}")
 }

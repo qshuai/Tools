c98d822caf2e2c76d14118fc9ddb7bb6d571af69
qshuai <qishuai231@gmail.com>
2019-08-16 11:58:34 +0800

    Add weekday calc and format tool
    eg: 2019/08/12 - 2019/08/16

---
+++ weekday/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	weekday := t.Weekday()
+
+	dayDiff := int(weekday - 1)
+	// 周日的情况
+	if dayDiff < 0 {
+		dayDiff = 6
+	}
+
+	startTime := t.Add(time.Duration(- dayDiff) * 24 * time.Hour)
+	endTime := t.Add(time.Duration(4 - dayDiff) * 24 * time.Hour)
+
+	fmt.Printf("%s - %s", startTime.Format("2006/01/02"), endTime.Format("2006/01/02"))
+}
